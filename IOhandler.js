1801ad8a8b69d352c67283e490a7c108bf6bb80c
armiyatvk <atavakoli7@my.bcit.ca>
2024-11-20 02:37:53 -0800

    first commit

--- IOhandler.js
+++ IOhandler.js
@@ -16,7 +16,6 @@ const pathIn = path.join(__dirname, 'myfile.zip');
 const pathOut = path.join(__dirname, 'unzipped');
 
 const unzip = async (pathIn, pathOut) => {
-  //study all these codes, as well as how FINALLY works...
   const zip = await yauzl.open(pathIn);
   try {
     await fs.mkdir(`${pathOut}`, { recursive: true });
@@ -37,8 +36,6 @@ const unzip = async (pathIn, pathOut) => {
   }
 };
 
-// unzip(pathIn, pathOut);
-
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
  *
@@ -55,9 +52,7 @@ const readDir = async (dir) => {
     console.log(error);
   }
 };
-// const dirPath = path.join(__dirname, 'unzipped');
-// console.log(readDir(dirPath));
-// readDir(dirPath)
+
 /**
  * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
@@ -69,8 +64,6 @@ const readDir = async (dir) => {
 
 
 const grayScale = (pathIn, pathOut) => {
-  // pathIn.forEach(imagePath => {
-    // const image = path.join(__dirname, 'unzipped', imagePath)
     createReadStream(pathIn)
       .pipe(
         new PNG({
@@ -78,16 +71,19 @@ const grayScale = (pathIn, pathOut) => {
         })
       )
       .on("parsed", function () {
-        for (var y = 0; y < this.height; y++) {
-          for (var x = 0; x < this.width; x++) {
-            var idx = (this.width * y + x) << 2;
+        for (let y = 0; y < this.height; y++) {
+          for (let x = 0; x < this.width; x++) {
+            const idx = (this.width * y + x) << 2;
 
-            const gray = 0.3 * this.data[idx] + 0.59 * this.data[idx + 1] + 0.11 * this.data[idx + 2];
+            const red = this.data[idx];
+            const green = this.data[idx + 1];
+            const blue =this.data[idx + 2];
+            
+            const gray = (red + green + blue) / 3;
 
-          // Set RGB channels to the grayscale value
-          this.data[idx] = gray; // Red channel
-          this.data[idx + 1] = gray; // Green channel
-          this.data[idx + 2] = gray; // Blue channel
+            this.data[idx] = gray;
+            this.data[idx + 1] = gray;
+            this.data[idx + 2] = gray;
           }
         }
 
@@ -95,13 +91,123 @@ const grayScale = (pathIn, pathOut) => {
       });
 
 };
-// const pathProcessed = path.join(__dirname, "grayscaled")
-// grayScale([ 'in.png', 'in1.png', 'in2.png' ], pathProcessed);
 
+const sepia = async (pathIn, pathOut) => {
+  createReadStream(pathIn)
+      .pipe(new PNG({}))
+      .on("parsed", function () {
+        for (let y = 0; y < this.height; y++) {
+          for (let x = 0; x < this.width; x++) {
+            const idx = (this.width * y + x) << 2;
+
+            const red = this.data[idx];
+            const green = this.data[idx + 1];
+            const blue =this.data[idx + 2];
+
+            const sepiaRed = Math.min(255, 0.393 * red + 0.769 * green + 0.189 * blue);
+            const sepiaGreen = Math.min(255, 0.349 * red + 0.686 * green + 0.168 * blue);
+            const sepiaBlue = Math.min(255, 0.272 * red + 0.534 * green + 0.131 * blue);
+
+
+            this.data[idx] = sepiaRed;
+            this.data[idx + 1] = sepiaGreen;
+            this.data[idx + 2] = sepiaBlue;
+          }
+        }
+
+        this.pack().pipe(createWriteStream(pathOut));
+      })
+}
+
+
+const dithering = async (pathIn, pathOut) => {
+  createReadStream(pathIn)
+  .pipe(new PNG({}))
+  .on("parsed", function () {
+    const idxShifter = (x, y) => (x + y * this.width) << 2;
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+          const idx = idxShifter(x, y);
+
+          let oldR = this.data[idx];
+          let oldG = this.data[idx + 1];
+          let oldB = this.data[idx + 2];
+
+          let newR = Math.round(2 * oldR / 255) * (255/2);
+          let newG = Math.round(2 * oldG / 255) * (255/2);
+          let newB = Math.round(2 * oldB / 255) * (255/2);
+          let gray = (newR + newG + newB) / 3
+
+          let quantErrR = oldR - newR;
+          let quantErrG = oldG - newG;
+          let quantErrB = oldB - newB;
+
+          this.data[idx] = newR;
+          this.data[idx + 1] = newG;
+          this.data[idx + 2] = newB; 
+
+          const distributeError = (xAdder, yAdder, ratio) => {
+            const newX = x + xAdder;
+            const newY = y + yAdder;
+            const newIdx = idxShifter(newX, newY);
+
+            this.data[newIdx] = Math.min(255, Math.max(0, this.data[newIdx] + quantErrR * ratio));
+            this.data[newIdx + 1] = Math.min(255, Math.max(0, this.data[newIdx + 1] + quantErrG * ratio));
+            this.data[newIdx + 2] = Math.min(255, Math.max(0, this.data[newIdx + 2] + quantErrB * ratio));
+          };
+
+          distributeError(1, 0, 7 / 16);
+          distributeError(-1, 1, 3 / 16);
+          distributeError(0, 1, 5 / 16);
+          distributeError(1, 1, 1 / 16);
+
+          // const idx1 = idxShifter(x + 1, y)
+          // const idx1Red = idx1 + quantErrR + 7/16;
+          // const idx1Green = idx1 + quantErrG + 7/16;
+          // const idx1Blue = idx1 + quantErrB + 7/16;
+          // this.data[idx1] = idx1Red;
+          // this.data[idx1 + 1] = idx1Green;
+          // this.data[idx1 + 2] = idx1Blue;
+
+
+          // const idx2 = idxShifter(x - 1, y + 1)
+          // const idx2Red = idx2 + quantErrR + 3/16;
+          // const idx2Green = idx2 + quantErrG + 3/16;
+          // const idx2Blue = idx2 + quantErrB + 3/16;
+          // this.data[idx2] = idx2Red;
+          // this.data[idx2 + 1] = idx2Green;
+          // this.data[idx2 + 2] = idx2Blue;
+
+
+          // const ix3 = idxShifter(x + 1, y)
+          // const idx3Red = ix3 + quantErrR + 5/16;
+          // const idx3Green = ix3 + quantErrG + 5/16;
+          // const idx3Blue = ix3 + quantErrB + 5/16;
+          // this.data[ix3] = idx3Red;
+          // this.data[ix3 + 1] = idx3Green;
+          // this.data[ix3 + 2] = idx3Blue;
+
+
+          // const idx4 = idxShifter(x + 1, y + 1)
+          // const idx4Red = idx4 + quantErrR + 1/16;
+          // const idx4Green = idx4 + quantErrG + 1/16;
+          // const idx4Blue = idx4 + quantErrB + 1/16;
+          // this.data[idx4] = idx4Red;
+          // this.data[idx4 + 1] = idx4Green;
+          // this.data[idx4 + 2] = idx4Blue;
+        }
+      }
+      this.pack().pipe(createWriteStream(pathOut));
+    })
+}
 
 
 module.exports = {
   unzip,
   readDir,
   grayScale,
+  sepia,
+  dithering,
 };
+
--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-// const path = require("path");
-
-// const IOhandler = require("./IOhandler");
-// const zipFilePath = path.join(__dirname, "myfile.zip");
-// const pathUnzipped = path.join(__dirname, "unzipped");
-// const pathProcessed = path.join(__dirname, "grayscaled");
-
-// const processFile = async () => {
-//     try {
-//         await IOhandler.unzip(zipFilePath, pathUnzipped);
-//         const pngFiles = await IOhandler.readDir(pathUnzipped);
-//         // const pngFiles = 
-//         await IOhandler.grayScale(pngFiles, pathProcessed);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// processFile();
-
-
 const path = require('path');
 const IOhandler = require('./IOhandler');
-
-// Paths for the zip file, unzipped folder, and processed images
+const readline = require('readline');
 const zipFilePath = path.join(__dirname, 'myfile.zip');
 const unzippedPath = path.join(__dirname, 'unzipped');
-const processedPath = path.join(__dirname, 'grayscaled');
+const processedPathGray = path.join(__dirname, 'grayscaled');
+const processedPathSepia = path.join(__dirname, 'sepia');
+const processedPathDither = path.join(__dirname, 'dithered');
 
-// Step 1: Unzip the file
-IOhandler.unzip(zipFilePath, unzippedPath)
-  .then(() => {
-    // Step 2: Read the directory for PNG files
-    return IOhandler.readDir(unzippedPath);
-  })
-  .then((files) => {
-    // Step 3: Apply grayscale to each PNG file
-    files.forEach((file) => {
-      const outputFile = path.join(processedPath, path.basename(file)); // Use the same name for the output file
-      IOhandler.grayScale(file, outputFile);
-    });
-  })
-  .catch((err) => {
-    console.error('Error in processing:', err);
-  });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const processImg = async () => {
+    try {
+        await IOhandler.unzip(zipFilePath, unzippedPath)
+        const files = await IOhandler.readDir(unzippedPath);
+        
+        rl.question(`Choose your filter between these option: grayscale , sepia , dithering `, async (input) => {
+            const chosenFilter = input.toLowerCase();
+            let newOutputPath;
+            let theChosenFunction;
+            if (chosenFilter === 'grayscale' || chosenFilter === 'sepia' || chosenFilter === 'dithering') {
+                for (let file of files) {
+                    if (chosenFilter === 'grayscale') {
+                        newOutputPath = path.join(processedPathGray, path.basename(file));
+                        theChosenFunction = IOhandler.grayScale(file, newOutputPath);
+                    } else if (chosenFilter === 'sepia') {
+                        newOutputPath = path.join(processedPathSepia, path.basename(file));
+                        theChosenFunction = IOhandler.sepia(file, newOutputPath);
+                    } else if (chosenFilter === 'dithering') {
+                        newOutputPath = path.join(processedPathDither, path.basename(file));
+                        theChosenFunction = IOhandler.dithering(file, newOutputPath);
+                    }
+                    rl.close();
+                }
+            } else {
+                console.log('You entered wrong input. Try again!');
+                rl.close();
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        rl.close();
+    }
+
+}
+
+processImg();
