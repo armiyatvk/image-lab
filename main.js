d2c50f03cb0787105ba0f1b8eb687ecfb20b2439
Alireza Tavakoli <110915889+armiyatvk@users.noreply.github.com>
2024-11-21 12:04:37 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const unzippedPath = path.join(__dirname, 'unzipped');
 const processedPathGray = path.join(__dirname, 'grayscaled');
 const processedPathSepia = path.join(__dirname, 'sepia');
 const processedPathDither = path.join(__dirname, 'dithered');
+const processedPathInvert = path.join(__dirname, 'inverted');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -18,19 +19,21 @@ const processImg = async () => {
         const files = await IOhandler.readDir(unzippedPath);
         
         rl.question(`Choose your filter between these option: grayscale , sepia , dithering `, async (input) => {
-            const chosenFilter = input.toLowerCase();
             let newOutputPath;
-            if (chosenFilter === 'grayscale' || chosenFilter === 'sepia' || chosenFilter === 'dithering' || chosenFilter === 'invert' || chosenFilter === "1" || chosenFilter === '2' || chosenFilter === '3' || chosenFilter === "4") {
-                for (let file of files) {
-                    if (chosenFilter === 'grayscale') {
+            if (input === 'grayscale' || input === 'sepia' || input === 'dithering' || input === 'invert' || input === "1" || input === '2' || input === '3' || input === "4") {
+                for (const file of files) {
+                    if (input === 'grayscale' || input === '1') {
                         newOutputPath = path.join(processedPathGray, path.basename(file));
                         IOhandler.grayScale(file, newOutputPath);
-                    } else if (chosenFilter === 'sepia') {
+                    } else if (input === 'sepia' || input === '2') {
                         newOutputPath = path.join(processedPathSepia, path.basename(file));
                         IOhandler.sepia(file, newOutputPath);
-                    } else if (chosenFilter === 'dithering') {
+                    } else if (input === 'dithering' || input === '3') {
                         newOutputPath = path.join(processedPathDither, path.basename(file));
                         IOhandler.dithering(file, newOutputPath);
+                    } else {
+                        newOutputPath = path.join(processedPathInvert, path.basename(file));
+                        IOhandler.invert(file, newOutputPath);
                     }
                     rl.close();
                 }
